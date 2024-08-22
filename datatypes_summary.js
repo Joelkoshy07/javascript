7ba8bd0e3df0fdad7297569226de0657649fa36d
Joel Mathew Koshy <83625560+Joelkoshy07@users.noreply.github.com>
2024-08-22 05:34:18 +0000

    datatypes summary

---
+++ datatypes_summary.js
@@ -0,0 +1,16 @@
+ // #Primitive(call by value)
+//7 types : string,number,boolean,null,undefined,symbol,bigint
+
+//#Reference or non primitive
+
+//Array,Objects,functions
+
+const heros = ["afsf", "eqed", "fefwf"] //array
+
+
+let myObj = {             //object
+    name: "joel"
+    age:65,
+}
+
+
