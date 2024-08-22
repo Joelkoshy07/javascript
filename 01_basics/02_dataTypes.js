3198ec3e263e382a0b7da162354c17c211c2f337
Joel Mathew Koshy <83625560+Joelkoshy07@users.noreply.github.com>
2024-08-22 01:16:16 +0000

    container config done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) //we are using nodejs, not browser
+
+let name = "hitesh"
+let age = 18
+let isloggedIn = false
+
+//number
+//bigint
+//string""
+//boolean -true/false
+//null - standalone value
+//undefined 
+//symbol - unique
+
+
+//object-non primitive
+
+
+console.log(typeof "hitesh")
+
+console.log(typeof null);//type of null is object
+
+
