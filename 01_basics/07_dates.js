f24241866bca93a7e88a7fb0c2407e7077b36b39
Joel Mathew Koshy <83625560+Joelkoshy07@users.noreply.github.com>
2024-08-22 11:59:21 +0000

    dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,14 @@
+//Dates
+
+//let myDate = new Date()
+//console.log(myDate.toLocaleString());
+//console.log(typeof myDate);
+
+
+//let myCreatedDate = new Date(2023 ,0 , 23)
+
+let myCreatedDate = new Date(2023 ,0 , 23, 5, 3)
+console.log(myCreatedDate.toLocaleString());
+
+let newDate = new Date()
+console.log(newDate);
