17339b4be539cb6633129dcc807be01154aac359
Joel Mathew Koshy <83625560+Joelkoshy07@users.noreply.github.com>
2025-04-01 10:55:16 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 144553
+let accountEmail = "joel@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState;
+//accountId = 2 
+accountEmail = "jmk@google.com"
+accountPassword = "212121"
+accountCity = "bengaluru"
+
+/* Prefer not to use var
+because of issue in block scope and functional scope*/
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+console.log(accountState);
+
+
+
+
